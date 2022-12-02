ccb1cf289100dc9693b7c519c5d6a5277cf2bcaa
Mantas Silanskas <mantas.silan@peplink.com>
2022-12-02 12:38:19 +0200

    Separated part one and two

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,13 +1,37 @@
-highest = 0
-current = 0
+def Calories1():
+    highest = 0
+    current = 0
 
-with open("input_day1.txt") as file:
-    for line in file:
-        if line == "\n":
-            if current > highest:
-                highest = current
-            current = 0
-            continue
-        current = current + int(line)
+    with open("input_day1.txt") as file:
+        for line in file:
+            if line == "\n":
+                if current > highest:
+                    highest = current
+                current = 0
+                continue
+            current = current + int(line)
 
-print(highest)
+    print(highest)
+
+def Calories2():
+    current = 0
+    total = 0
+    calories = []
+
+    with open("input_day1.txt") as file:
+        for line in file:
+            if line == "\n":
+                calories.append(current)
+                current = 0
+                continue
+            current = current + int(line)
+
+    top = sorted(calories,reverse=True)[:3]
+
+    for i in top:
+        total += i
+
+    print(total)
+
+Calories1()
+Calories2()
