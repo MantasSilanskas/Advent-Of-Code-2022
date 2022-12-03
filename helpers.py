e7eff8a81bfe39a1817c2c97e97a4ce6815cab2a
MantasSil <mantas.silanskas.ms@gmail.com>
2022-12-03 12:47:53 +0200

    Added a few helpers function

---
+++ helpers.py
@@ -0,0 +1,22 @@
+def ReadFileByLine(fileName, lines):
+    lines = []
+    with open(fileName) as file:
+        for line in file:
+            lines.append(line)
+
+    return lines
+
+
+def SortAscending(list):
+    return sorted(list)
+
+
+def SortDescending(list):
+    return sorted(list, reverse=True)
+
+
+def SplitStringInHalf(string):
+    s1 = string[:len(string)//2]
+    s2 = string[len(string)//2:]
+
+    return s1, s2
