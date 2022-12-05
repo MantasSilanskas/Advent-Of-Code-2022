f6e6dfa7e27482218b755ed3b4610a3ab7b7796c
Mantas Silanskas <mantas.silan@peplink.com>
2022-12-05 10:44:39 +0200

    Separated part one and two in code

--- Day05/main.py
+++ Day05/main.py
@@ -1,11 +1,22 @@
-moves = []
-
 boardDemo = [
     [' '],
     ['Z','N'],
     ['M','C','D'],
     ['P']
     ]
+
+board1 = [
+    [' '],
+    ['Z','T','F','R','W','J','G'],
+    ['G','W','M'],
+    ['J','N','H','G'],
+    ['J','R','C','N','W'],
+    ['W','F','S','B','G','Q','V','M'],
+    ['S','R','T','D','V','W','C'],
+    ['H','B','N','C','D','Z','G','V'],
+    ['S','J','N','M','G','C'],
+    ['G','P','N','W','C','J','D','L'],
+]
 
 board = [
     [' '],
@@ -20,30 +31,64 @@ board = [
     ['G','P','N','W','C','J','D','L'],
 ]
 
-with open("input.txt") as file:
-    for line in file.readlines():
-        element = []
-        for token in line.strip().split(" "):
-            element.append(token)
-        moves.append(element)
+def CrateMover9000():
+    moves = []
 
-for move in moves:
-    boxes = []
+    with open("input.txt") as file:
+        for line in file.readlines():
+            element = []
+            for token in line.strip().split(" "):
+                element.append(token)
+            moves.append(element)
 
-    startStack = int(move[3])
-    endStack = int(move[5])
+    for move in moves:
+        boxes = []
 
-    for times in range(int(move[1])):
-        box = board[startStack][-1]
-        del(board[startStack][-1])
+        startStack = int(move[3])
+        endStack = int(move[5])
 
-        boxes.insert(0,box)
+        for times in range(int(move[1])):
+            box = board1[startStack][-1]
+            del(board1[startStack][-1])
 
-    for box in boxes:
-        board[endStack].append(box)
+            board1[endStack].append(box)
 
-solution = ''
-for box in board:
-    solution += box[-1]
+    solution = ''
+    for box in board1:
+        solution += box[-1]
 
-print(solution)
+    print("Solution for CrateMover9000: ",solution)   
+
+def CrateMover9001():
+    moves = []
+
+    with open("input.txt") as file:
+        for line in file.readlines():
+            element = []
+            for token in line.strip().split(" "):
+                element.append(token)
+            moves.append(element)
+
+    for move in moves:
+        boxes = []
+
+        startStack = int(move[3])
+        endStack = int(move[5])
+
+        for times in range(int(move[1])):
+            box = board[startStack][-1]
+            del(board[startStack][-1])
+
+            boxes.insert(0,box)
+
+        for box in boxes:
+            board[endStack].append(box)
+
+    solution = ''
+    for box in board:
+        solution += box[-1]
+
+    print("Solution for CrateMover9001: ",solution)
+
+CrateMover9000()
+CrateMover9001()
