b7464e87d04cd197b268f8a049c4cdc00638ba10
Mantas Silanskas <mantas.silan@peplink.com>
2022-12-06 07:59:33 +0200

    Day 06 of Advent of Code

---
+++ Day06/main.py
@@ -0,0 +1,23 @@
+def MarkerDetection():
+    with open("input.txt") as file:
+        data = file.read().strip()
+
+    
+    for i in range(4, len(data)):
+        if len(set(data[i-4:i])) == 4:
+            print("Character processed before first marker: ",i)
+            break
+
+def StartOfMessageMarker():
+    with open("input.txt") as file:
+        data = file.read().strip()
+
+    
+    for i in range(14, len(data)):
+        if len(set(data[i-14:i])) == 14:
+            print("Character processed before start of message marker: ",i)
+            break
+
+
+MarkerDetection()
+StartOfMessageMarker()
