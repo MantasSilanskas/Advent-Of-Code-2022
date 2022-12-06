1c836ff8fae32e132d6169b43f326b8e1418aecf
Mantas Silanskas <mantas.silan@peplink.com>
2022-12-06 09:01:26 +0200

    Code formatting

--- Day06/main.py
+++ Day06/main.py
@@ -2,7 +2,6 @@ def MarkerDetection():
     with open("input.txt") as file:
         data = file.read().strip()
 
-    
     for i in range(4, len(data)):
         if len(set(data[i-4:i])) == 4:
             print("Character processed before first marker: ",i)
@@ -12,12 +11,10 @@ def StartOfMessageMarker():
     with open("input.txt") as file:
         data = file.read().strip()
 
-    
     for i in range(14, len(data)):
         if len(set(data[i-14:i])) == 14:
             print("Character processed before start of message marker: ",i)
             break
-
 
 MarkerDetection()
 StartOfMessageMarker()
